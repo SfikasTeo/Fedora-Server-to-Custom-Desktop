f8db538ede5f6408fd55129cb24968348abdea4b
Sfikas <65178946+SfikasTeo@users.noreply.github.com>
2022-07-07 01:13:47 +0300

    Update FedoraBasicProgramms.sh

--- FedoraBasicProgramms.sh
+++ FedoraBasicProgramms.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 sudo dnf install fira-code-fonts kitty nitrogen picom Xorg xorg-x11-xinit xorg-x11-xauth gwe xorg-x11-drivers dnf-plugins-core \
-                 bspwm sxhkd polybar rofi pcmanfm fish starship gcc python-unversioned-command timeshift xrandr pulseaudio
+                 bspwm sxhkd polybar rofi pcmanfm fish starship gcc python-unversioned-command timeshift xrandr pulseaudio flameshot
  
