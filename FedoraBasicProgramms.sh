d1e377a4e0ad6d2dc227622dd222f1e9d064d653
Sfikas <65178946+SfikasTeo@users.noreply.github.com>
2022-07-06 16:41:04 +0300

    Create FedoraBasicProgramms.sh

---
+++ FedoraBasicProgramms.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+sudo dnf install fira-code-fonts kitty nitrogen picom Xorg xorg-x11-xinit xorg-x11-xauth gwe xorg-x11-drivers dnf-plugins-core bspwm sxhkd polybar rofi pcmanfm fish starship
