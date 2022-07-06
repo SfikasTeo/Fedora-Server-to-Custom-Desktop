81cfdfec2fe68cd094fefd2aa9ab5943744ece63
Sfikas <65178946+SfikasTeo@users.noreply.github.com>
2022-07-07 01:23:25 +0300

    Update FedoraBasicProgramms.sh

--- FedoraBasicProgramms.sh
+++ FedoraBasicProgramms.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-sudo dnf install fira-code-fonts kitty nitrogen picom Xorg xorg-x11-xinit xorg-x11-xauth gwe xorg-x11-drivers dnf-plugins-core \
+sudo dnf install fira-code-fonts kitty nitrogen picom Xorg xorg-x11-xinit xorg-x11-xauth gwe xorg-x11-drivers dnf-plugins-core vlc \
                  bspwm sxhkd polybar rofi pcmanfm fish starship gcc python-unversioned-command timeshift xrandr pulseaudio flameshot
  
