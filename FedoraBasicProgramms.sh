46e113e80eb3810cf94395a1cf3ba546d87a4f12
Sfikas <65178946+SfikasTeo@users.noreply.github.com>
2022-07-07 10:29:17 +0300

    Update FedoraBasicProgramms.sh

--- FedoraBasicProgramms.sh
+++ FedoraBasicProgramms.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
-sudo dnf install fira-code-fonts kitty nitrogen picom Xorg xorg-x11-xinit xorg-x11-xauth gwe xorg-x11-drivers dnf-plugins-core vlc \
+
+#Enable the rpmfusion depository for vlc or ditch vlc for mplayer2
+#sudo dnf install https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm vlc
+sudo dnf install fira-code-fonts kitty nitrogen picom Xorg xorg-x11-xinit xorg-x11-xauth gwe xorg-x11-drivers dnf-plugins-core \
                  bspwm sxhkd polybar rofi pcmanfm fish starship gcc python-unversioned-command timeshift xrandr pulseaudio flameshot
  
