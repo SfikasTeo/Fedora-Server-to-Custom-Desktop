d8c5e955084a370a50af5ae02896bbfc426acd8f
Sfikas <65178946+SfikasTeo@users.noreply.github.com>
2022-07-08 12:16:59 +0300

    Update and rename CloneLy.sh to LyInstallation.sh

--- LyInstallation.sh
+++ LyInstallation.sh
@@ -2,7 +2,11 @@
 cd ~
 git clone --recurse-submodules https://github.com/nullgemm/ly
 cd ly
+
 sudo make
 sudo make install
+
+#enable the ly deamon
 sudo systemctl enable ly.service
 sudo systemctl disable getty@tty2.service
+#sudo systemctl set-default graphical.target
