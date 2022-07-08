a01581441259879f37fce6566d6cac7a24247f54
Sfikas <65178946+SfikasTeo@users.noreply.github.com>
2022-07-08 12:10:07 +0300

    Update FedoraBasicProgramms.sh

--- FedoraBasicProgramms.sh
+++ FedoraBasicProgramms.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+#Scipt for installing the most basic programms the fedora Server needs. It is mandatoty to select the network packages as well during installation
+
 # Check if Script is Run as Root
 if [[ $EUID -ne 0 ]]; then
   echo "You must be a root user to run this script, please run sudo ./install.sh" 2>&1
@@ -16,7 +18,7 @@ dnf install kitty nitrogen picom bspwm sxhkd polybar rofi pcmanfm gcc python-unv
 sudo dnf install fish starship flameshot fira-code-fonts zip unzip wget
 
 #X11
-dnf insstall Xorg xorg-x11-xinit xorg-x11-xauth xorg-x11-drivers xrandr
+dnf insstall Xorg xorg-x11-xinit xorg-x11-xauth xorg-x11-drivers xrandr arandr
 
 #Breave-bin
 dnf install dnf-plugins-core
