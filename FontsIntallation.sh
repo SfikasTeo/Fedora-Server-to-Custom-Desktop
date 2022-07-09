b6ddccc11e40386e8137da72c2fad24d33713c53
Sfikas <65178946+SfikasTeo@users.noreply.github.com>
2022-07-09 12:55:37 +0300

    Update FontsIntallation.sh

--- FontsIntallation.sh
+++ FontsIntallation.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 #Scipt is used to install the wanted fonts (wget is mandatory)
 
+# Check if Script is Run as Root
+if [[ $EUID -ne 0 ]]; then
+  echo "You must be a root user to run this script, please use sudo" 2>&1
+  exit 1
+fi
 # Installing fonts
-dnf install fontawesome-fonts fontawesome-fonts-web
+sudo dnf install fontawesome-fonts fontawesome-fonts-web
 wget https://github.com/ryanoasis/nerd-fonts/releases/download/v2.1.0/FiraCode.zip
 unzip FiraCode.zip -d /usr/share/fonts
 wget https://github.com/ryanoasis/nerd-fonts/releases/download/v2.1.0/Meslo.zip
