8a57e4b51b240f30c4a766ae2c1b41d3f867d575
Sfikas <65178946+SfikasTeo@users.noreply.github.com>
2022-07-11 13:43:00 +0300

    Update BinInstallation.sh

--- BinInstallation.sh
+++ BinInstallation.sh
@@ -15,7 +15,7 @@ dnf update -y
 dnf install kitty feh picom polybar rofi pcmanfm gcc python-unversioned-command timeshift
 
 #fish shell + less important binaries
-sudo dnf install fish starship flameshot fira-code-fonts zip unzip wget bpytop lm_sensors
+sudo dnf install fish flameshot fira-code-fonts zip unzip wget bpytop lm_sensors
 
 #X11
 dnf insstall Xorg xorg-x11-xinit xorg-x11-xauth xorg-x11-drivers xrandr arandr xsensors
