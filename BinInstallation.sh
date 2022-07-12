f48ffe9c2c8d7f4df2aee8d1faefb4cc9bb32edd
Sfikas <65178946+SfikasTeo@users.noreply.github.com>
2022-07-13 01:24:55 +0300

    Update BinInstallation.sh

--- BinInstallation.sh
+++ BinInstallation.sh
@@ -12,16 +12,19 @@ fi
 dnf update -y
 
 #Basic programms
-dnf install kitty feh picom polybar rofi pcmanfm gcc python-unversioned-command timeshift
+dnf install -y kitty feh picom polybar rofi pcmanfm gcc python-unversioned-command timeshift
 
-#fish shell + less important binaries
-sudo dnf install fish flameshot fira-code-fonts zip unzip wget bpytop lm_sensors
+#less important binaries
+dnf install -y fish flameshot zip unzip wget curl bpytop lm_sensors
+
+#Install basic fonts
+dnf install -y mscore-fonts fira-code-fonts
 
 #X11
-dnf insstall Xorg xorg-x11-xinit xorg-x11-xauth xorg-x11-drivers xrandr arandr xsensors
+dnf insstall -y Xorg xorg-x11-xinit xorg-x11-xauth xorg-x11-drivers xrandr arandr
 
 #Breave-bin
 dnf install dnf-plugins-core
-sudo dnf config-manager --add-repo https://brave-browser-rpm-release.s3.brave.com/x86_64/ #add brave repository
-sudo rpm --import https://brave-browser-rpm-release.s3.brave.com/brave-core.asc #add repository key
-sudo dnf install brave-browser
+dnf config-manager --add-repo https://brave-browser-rpm-release.s3.brave.com/x86_64/ #add brave repository
+rpm --import https://brave-browser-rpm-release.s3.brave.com/brave-core.asc #add repository key
+dnf install brave-browser
